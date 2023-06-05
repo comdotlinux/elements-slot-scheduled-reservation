19c0937568ee40bf98fc83cfab89ca9e0ec785d5
Guruprasad Kulkarni <guru@linux.com>
2023-06-05 11:51:15 +0200

    Increase Screen and Viewport size also take screenshot not video if success

--- cypress/e2e/reserve-slot/reserve.cy.js
+++ cypress/e2e/reserve-slot/reserve.cy.js
@@ -1,5 +1,6 @@
-describe('Elemets Gym', () => {
+describe('Elements Gym', () => {
 	it('Reserve Slot for tomorrow', () => {
+		cy.viewport(1920, 1080)
 		cy.visit('https://slots.elements.com/', { timeout: 20_000 })
 		cy.get('#input_login_email').type(Cypress.env('ELEMENTS_EMAIL'))
 		cy.get('#input_login_password').type(Cypress.env('ELEMENTS_PASSWORD'))
@@ -11,5 +12,13 @@ describe('Elemets Gym', () => {
 			.find('a.button')
 			.contains('Platz reservieren')
 			.click()
+			
+		cy.wait(100)
+		cy.screenshot('slot-reserved', {capture: 'runner'})
+		cy.get('div.course-table', { timeout: 20_000 })
+			.find('a.button')
+			.should('have.text', 'Reservierung löschen')
+
+		cy.wait(1_000)
 	})
   })
